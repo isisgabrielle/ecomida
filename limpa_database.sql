a4960e2b48eb3d35d21ee9f6662d19a1b9af9154
Isis Menezes <isisalternativo@gmail.com>
2024-11-10 03:23:15 -0300

    atualização padrao de nomeclatura

--- limpa_database.sql
+++ limpa_database.sql
@@ -1,66 +1,66 @@
 TRUNCATE TABLE 
-	Conteudos_interface,
+    conteudos_interface,
     penalidades,
-	chat,
-    suporte,
+    chats,
+    suportes,
     imagens,
     produtos_pedidos,
-    estoque,
+    estoques,
     ecomidamarket,
     pedidos,
     promocoes,
     favoritos,
     avaliacoes,
-    barraquinha,
-    visitacao,
-    correcao_estoque,
-    horario_funcionamento,
-    beneficiamento,
-    cadastro_agricultor,
-    propriedade,
+    barraquinhas,
+    visitacoes,
+    correcoes_estoque,
+    horarios_funcionamento,
+    beneficiamentos,
+    cadastros_agricultores,
+    propriedades,
     documentos,
-    alimento,
-	documentos_educativos,
+    alimentos,
+    documentos_educativos,
     tipos_documentos,
-    tipos_de_penalidade,
-    subcategoria_popular,
-    subcategoria_cientifica,
-    categoria_popular,
-    categoria_cientifica,
-    produto,
-    endereco,
+    tipos_de_penalidades,
+    subcategorias_populares,
+    subcategorias_cientificas,
+    categorias_populares,
+    categorias_cientificas,
+    produtos,
+    enderecos,
     feirinhas,
     medidas_caseiras,
     medidas_padrao,
-    usuario;
+    usuarios;
 
 ALTER SEQUENCE usuarioid_seq RESTART WITH 1;
-ALTER SEQUENCE medidas_caseirasid_seq RESTART WITH 1;
-ALTER SEQUENCE medida_padraoid_seq RESTART WITH 1;
+ALTER SEQUENCE medidacaseiraid_seq RESTART WITH 1;
+ALTER SEQUENCE medidapadraoid_seq RESTART WITH 1;
 ALTER SEQUENCE tipopenalidadeid_seq RESTART WITH 1;
-ALTER SEQUENCE penalidadesid_seq RESTART WITH 1;
+ALTER SEQUENCE penalidadeid_seq RESTART WITH 1;
 ALTER SEQUENCE barraquinhaid_seq RESTART WITH 1;
-ALTER SEQUENCE feirinhasid_seq RESTART WITH 1;
+ALTER SEQUENCE feirinhaid_seq RESTART WITH 1;
 ALTER SEQUENCE categoriapopularid_seq RESTART WITH 1;
 ALTER SEQUENCE categoriacientificaid_seq RESTART WITH 1;
 ALTER SEQUENCE alimentoid_seq RESTART WITH 1;
 ALTER SEQUENCE doceducativoid_seq RESTART WITH 1;
-ALTER SEQUENCE subcategoriaid_seq RESTART WITH 1;
+ALTER SEQUENCE subcategoriapopularid_seq RESTART WITH 1;
 ALTER SEQUENCE subcategoriacientificaid_seq RESTART WITH 1;
 ALTER SEQUENCE produtoid_seq RESTART WITH 1;
-ALTER SEQUENCE imagensid_seq RESTART WITH 1;
-ALTER SEQUENCE pedidosid_seq RESTART WITH 1;
-ALTER SEQUENCE promocoesid_seq RESTART WITH 1;
+ALTER SEQUENCE imagemid_seq RESTART WITH 1;
+ALTER SEQUENCE pedidoid_seq RESTART WITH 1;
+ALTER SEQUENCE promocaoid_seq RESTART WITH 1;
 ALTER SEQUENCE ecomidamarketid_seq RESTART WITH 1;
 ALTER SEQUENCE enderecoid_seq RESTART WITH 1;
-ALTER SEQUENCE avaliacoesid_seq RESTART WITH 1;
+ALTER SEQUENCE avaliacaoid_seq RESTART WITH 1;
 ALTER SEQUENCE horariofuncionamentoid_seq RESTART WITH 1;
 ALTER SEQUENCE chatid_seq RESTART WITH 1;
 ALTER SEQUENCE suporteid_seq RESTART WITH 1;
-ALTER SEQUENCE favoritosid_seq RESTART WITH 1;
-ALTER SEQUENCE produtospedidosid_seq RESTART WITH 1;
+ALTER SEQUENCE favoritoid_seq RESTART WITH 1;
+ALTER SEQUENCE produtopedidoid_seq RESTART WITH 1;
 ALTER SEQUENCE estoqueid_seq RESTART WITH 1;
-ALTER SEQUENCE cadastro_agricultorid_seq RESTART WITH 1;
+ALTER SEQUENCE cadastroagricultorid_seq RESTART WITH 1;
 ALTER SEQUENCE beneficiamentoid_seq RESTART WITH 1;
 ALTER SEQUENCE documentoid_seq RESTART WITH 1;
 ALTER SEQUENCE propriedadeid_seq RESTART WITH 1;
@@ -68,3 +68,4 @@ ALTER SEQUENCE visitaid_seq RESTART WITH 1;
 ALTER SEQUENCE correcaoestoqueid_seq RESTART WITH 1;
 ALTER SEQUENCE tipodocid_seq RESTART WITH 1;
 ALTER SEQUENCE conteudo_seq RESTART WITH 1;
+
